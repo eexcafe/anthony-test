4f8ca521e993b06713542ec11e42993e6a18b56f
王雪 <devel0per@yeah.net>
2022-11-05 12:03:49 +0000

    111

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,6 @@ import type { GetStaticProps, NextPage } from "next";
 import { read } from "to-vfile";
 import { matter, VFile } from "vfile-matter";
 import fs from "fs";
-import { admin } from "@/lib/firebase-admin";
-import ProgressBar from "@/components/interface/blog/ProgressBar";
-import Portfolio from "@/components/interface/index/Portfolio";
 
 type Post = {
   slug: string;
@@ -25,16 +22,9 @@ type Props = {
 };
 
 const Home: NextPage<Props> = ({ posts, mostViews }) => {
-  const sorted = posts.sort((post1, post2) => post2.created - post1.created);
-  const topPost = posts.find((post) => post.slug == mostViews);
-  const recentPost = topPost == sorted[0] ? sorted[1] : sorted[0];
-
   return (
     <Layout>
       <Welcome />
-      <Featured posts={[topPost!, recentPost]} />
-      <Portfolio />
-      <Subscribe />
     </Layout>
   );
 };
@@ -42,32 +32,7 @@ const Home: NextPage<Props> = ({ posts, mostViews }) => {
 export const getStaticProps: GetStaticProps<Props> = async () => {
   // MDX text - can be from a local file, database, anywhere
   const posts: Array<Post> = [];
-  const dir: Dirent[] = fs.readdirSync("./posts", { withFileTypes: true });
-  const mostViews = (
-    await admin
-      .firestore()
-      .collection("post")
-      .orderBy("views", "desc")
-      .limit(1)
-      .get()
-  ).docs[0].id;
-  for (const file of dir) {
-    if (file.isDirectory()) continue;
-    const source: VFile = await read(`./posts/${file.name}`);
-    const content = source.value
-      .toString()
-      .trim()
-      .slice(source.value.toString().trim().indexOf("---", 3) + 3);
-    matter(source, { strip: true });
-    const created = fs.statSync(`./posts/${file.name}`).birthtime;
-
-    posts.push({
-      slug: file.name.slice(0, file.name.lastIndexOf(".")),
-      frontmatter: source.data.matter as Frontmatter,
-      content: (content.match(/[A-Za-z0-9 _.,!"?']*/g) || []).join(" "),
-      created: created.getTime(),
-    });
-  }
+  const mostViews: any = [];
   return { props: { posts, mostViews } };
 };
 
